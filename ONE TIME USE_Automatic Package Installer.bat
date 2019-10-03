b67e0651867d89c017662d11a4f40af207cfad76
shanej199127 <45632421+shanej199127@users.noreply.github.com>
2019-10-03 11:33:27 +0200

    Update ONE TIME USE_Automatic Package Installer.bat

--- ONE TIME USE_Automatic Package Installer.bat
+++ ONE TIME USE_Automatic Package Installer.bat
@@ -4,6 +4,4 @@ call %root%\Scripts\activate.bat %root%
 
 call conda install pytorch torchvision cudatoolkit=9.0 -c pytorch
 call pip install PIL
-call pip install PySimpleGUI
-call pip install Tkinter
 call pip install mlxtend
